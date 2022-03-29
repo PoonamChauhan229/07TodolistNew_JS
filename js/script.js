3f319faa5eb4f858b6a4f3134e4987f66e0670f4
Poonam <poonam.chauhan229@gmail.com>
2022-03-29 14:57:45 +0530

    drag and drop

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ var title,date,desc,count=0;
 var pendingTask=document.getElementById('pendingTask')
 var activeTask=document.getElementById('activeTask')
 var completeTask=document.getElementById('completeTask')
-
+var choice=document.getElementsByClassName('choice')
+var dragItem=null;
 function addTask(){
     title=document.getElementById('taskTitle');
+    
     date=document.getElementById('taskDate');
     desc=document.getElementById('taskDesc');
     count++;
@@ -23,6 +25,7 @@ function createDiv(id,type){
     // alert(type)
     var newDiv=document.createElement('div')
     newDiv.id="taskDiv"+id;
+    newDiv.draggable=true;
     newDiv.classList="newDiv"
     newDiv.innerHTML="<span>Title:</span><input type='text' id='title"+id+"' class='text'value='"+title.value+"'readOnly><br>";
     newDiv.innerHTML+="<span>Date:</span><input type='text' id='date"+id+"' class='text'value='"+date.value+"'readOnly><br>";
@@ -34,13 +37,54 @@ function createDiv(id,type){
     //newDiv.innerHTML+=`<input type='button' onclick="moveTask('${id}','${type}')" value='Move'>`
     newDiv.innerHTML+=`<i class="fa-solid fa-angles-left" onclick="moveLeft('${id}','${type}')"></i>`
     newDiv.innerHTML+=`<i class="fa-solid fa-angles-right" onclick="moveRight('${id}','${type}')"></i>`
-    
+  
     if(type=="pending")
     pendingTask.appendChild(newDiv)
     else if(type=="active")
     activeTask.appendChild(newDiv)
     else 
     completeTask.appendChild(newDiv)
+
+    var dragDiv=document.getElementsByClassName('newDiv')
+    console.log(dragDiv);
+
+    for(var i of dragDiv){
+        i.addEventListener('dragstart',dragStart)
+        console.log(i)
+        i.addEventListener('dragend',dragEnd)
+    }
+    function dragStart(){
+        dragItem=this;
+        console.log(dragItem);
+        setTimeout(()=>this.style.display="block",0)
+    }
+    function dragEnd(){        
+        setTimeout(()=>this.style.display="block",0)
+        dragItem=null;
+    }
+    for(j of choice){
+        j.addEventListener('dragover',dragOver);
+        j.addEventListener('dragenter',dragEnter);
+        j.addEventListener('dragleave',dragLeave);
+        j.addEventListener('drop',dragDrop);
+    }
+    function dragDrop(e){
+        this.append(dragItem);
+    }
+    function dragOver(e){
+        e.preventDefault();
+        this.style.border="2px dotted cyan"
+    }
+    function dragEnter(e){
+        e.preventDefault();
+    }
+    function dragLeave(){
+         this.style.border="none"
+    }
+
+
+
+
 }
 //edit task
 function editTask(id){
@@ -48,6 +92,7 @@ function editTask(id){
     document.getElementById("title"+id).removeAttribute('readonly');
     document.getElementById("date"+id).removeAttribute('readonly');
     document.getElementById("desc"+id).removeAttribute('readonly');
+    
 
 }
 //remove task div
@@ -57,9 +102,10 @@ function removeDiv(divId){
 
 }
 function getValues(id){
-    title=document.getElementById('title'+id).value;
-    desc=document.getElementById('desc'+id).value;
-    date=document.getElementById("date"+id).value;    
+    title=document.getElementById('title'+id);
+    desc=document.getElementById('desc'+id);
+    date=document.getElementById("date"+id); 
+    console.log(title)
 }
 //move task
 function moveRight(id,type){
@@ -87,3 +133,4 @@ function moveLeft(id,type){
     document.getElementById("taskDiv"+id).remove();
     
 }
+
